f3f50f7353382240905b70e9c3163e03b2b877ca
Emmanuel Vankerkore <emmanuel.poejava@gmail.com>
2021-02-25 22:55:12 +0100

    Mise en place de l'API json-server

--- src/app/features/formateur/liste-formateur/liste-formateur.component.ts
+++ src/app/features/formateur/liste-formateur/liste-formateur.component.ts
@@ -9,22 +9,7 @@ import { FormateurHttpService } from 'src/app/services/formateur/formateur-http.
 })
 export class ListeFormateurComponent implements OnInit {
 
-  formateurs:Formateur[] = [
-    { id:1,
-      nom:"Dujardin",
-      prenom:"Toto",
-      dateNaissance:"12/01/1887",
-      entreprise:"Cupgemini",
-      telephone:"04786454XX",
-      email:"tdujardin@gmail.com"},
-    { id:2,
-      nom:"Dupres",
-      prenom:"Tutu",
-      dateNaissance:"11/08/1999",
-      entreprise:"Sogeto",
-      telephone:"04711154XX",
-      email:"tdupres@gmail.com"}
-  ]
+  formateurs:Formateur[] = []
 
   constructor(private formateurHttpService:FormateurHttpService) { }
 
