2a1fbe4346a7a5a425ff4306fd81b6073d4bfee8
Emmanuel Vankerkore <emmanuel.poejava@gmail.com>
2021-02-25 22:21:42 +0100

    Première version du liste-formateur.components.ts

--- src/app/features/formateur/liste-formateur/liste-formateur.component.ts
+++ src/app/features/formateur/liste-formateur/liste-formateur.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Formateur } from 'src/app/models/Formateur';
+import { FormateurHttpService } from 'src/app/services/formateur/formateur-http.service';
 
 @Component({
   selector: 'app-liste-formateur',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeFormateurComponent implements OnInit {
 
-  constructor() { }
+  formateurs:Formateur[] = [
+    { id:1,
+      nom:"Dujardin",
+      prenom:"Toto",
+      dateNaissance:"12/01/1887",
+      entreprise:"Cupgemini",
+      telephone:"04786454XX",
+      email:"tdujardin@gmail.com"},
+    { id:2,
+      nom:"Dupres",
+      prenom:"Tutu",
+      dateNaissance:"11/08/1999",
+      entreprise:"Sogeto",
+      telephone:"04711154XX",
+      email:"tdupres@gmail.com"}
+  ]
+
+  constructor(private formateurHttpService:FormateurHttpService) { }
 
   ngOnInit(): void {
+    // this.formateurHttpService.findAll().subscribe(res=>{
+    //   this.formateurs = res
+    // })
   }
+
+
 
 }
