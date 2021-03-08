646300146be19244ed05e4b24b2a54d9d863e908
Emmanuel Vankerkore <emmanuel.poejava@gmail.com>
2021-03-08 20:58:23 +0100

    La boucle sur les réseaux sociaux est fonctionnelle

--- src/app/commons/footers/reseaux-sociaux/reseaux-sociaux.component.ts
+++ src/app/commons/footers/reseaux-sociaux/reseaux-sociaux.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { reseau } from 'src/app/models/Reseau';
 
 @Component({
   selector: 'app-reseaux-sociaux',
@@ -7,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReseauxSociauxComponent implements OnInit {
 
-  // listeReseauxSociaux:reseau[] = [
-  //   {nom:"Twitter", lienImage: "M", lienHyperTexte:"Roux"},
-  //   {nom:"Facebook", lienImage: "", lienHyperTexte:"Roux"}
-  // ]
+  listeReseauxSociaux:reseau[] = [
+    {nom:"Twitter", 
+      lienImage: "https://th.bing.com/th/id/OIP.1nFvGnrJ53FAqIefnNCAWwHaHa?w=221&h=220&c=7&o=5&pid=1.7", 
+      lienHyperTexte:"https://twitter.com/?lang=fr"},
+    {nom:"Facebook", 
+      lienImage: "https://th.bing.com/th/id/OIP.67erQrAo-zy3lbAatv9iHQHaHa?w=186&h=186&c=7&o=5&pid=1.7",
+      lienHyperTexte:"https://fr-fr.facebook.com/"}
+  ]
 
   constructor() { }
 
